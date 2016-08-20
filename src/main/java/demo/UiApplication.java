432cc488eb644b920fa85a4faf52832509732c74
Suhael Akhtar <suhael.akhtar@maersk.com>
2016-08-20 13:19:25 +0100

    form auth

--- src/main/java/demo/UiApplication.java
+++ src/main/java/demo/UiApplication.java
@@ -1,17 +1,29 @@
 package demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 @SpringBootApplication
 @RestController
 public class UiApplication {
+
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
 
 	@RequestMapping("/resource")
 	public Map<String,Object> home() {
@@ -23,6 +35,24 @@ public class UiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UiApplication.class, args);
+	}
+
+	@Configuration
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			http
+				.httpBasic().and()
+				.authorizeRequests()
+					.antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
+					.anyRequest().authenticated()
+					.and()
+				.csrf()
+					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+			// @formatter:on
+		}
 	}
 
 }
--- src/test/java/demo/ApplicationTests.java
+++ src/test/java/demo/ApplicationTests.java
@@ -1,7 +1,5 @@
 package demo;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.context.embedded.LocalServerPort;
@@ -12,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 public class ApplicationTests {
@@ -19,10 +19,35 @@ public class ApplicationTests {
 	@LocalServerPort
 	private int port;
 
+	private TestRestTemplate template = new TestRestTemplate();
+
 	@Test
-	public void homePageProtected() {
-		ResponseEntity<String> response = new TestRestTemplate().getForEntity("http://localhost:" + port + "/", String.class);
+	public void homePageLoads() {
+		ResponseEntity<String> response = template.getForEntity("http://localhost:"
+				+ port + "/", String.class);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+	}
+
+	@Test
+	public void userEndpointProtected() {
+		ResponseEntity<String> response = template.getForEntity("http://localhost:"
+				+ port + "/user", String.class);
 		assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+	}
+
+	@Test
+	public void resourceEndpointProtected() {
+		ResponseEntity<String> response = template.getForEntity("http://localhost:"
+				+ port + "/resource", String.class);
+		assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+	}
+
+	@Test
+	public void loginSucceeds() {
+		TestRestTemplate template = new TestRestTemplate("user", "password");
+		ResponseEntity<String> response = template.getForEntity("http://localhost:" + port
+				+ "/user", String.class);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
 	}
 
 }
