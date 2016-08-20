b25ee053590210d22497b3b68936e873722b7c81
Suhael Akhtar <suhael.akhtar@maersk.com>
2016-08-20 14:39:21 +0100

    split front end and backend

--- ui/src/main/java/demo/UiApplication.java
+++ ui/src/main/java/demo/UiApplication.java
@@ -1,5 +1,7 @@
 package demo;
 
+import java.security.Principal;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
@@ -11,11 +13,6 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
 @SpringBootApplication
 @RestController
 public class UiApplication {
@@ -23,14 +20,6 @@ public class UiApplication {
 	@RequestMapping("/user")
 	public Principal user(Principal user) {
 		return user;
-	}
-
-	@RequestMapping("/resource")
-	public Map<String,Object> home() {
-		Map<String,Object> model = new HashMap<String,Object>();
-		model.put("id", UUID.randomUUID().toString());
-		model.put("content", "Hello World");
-		return model;
 	}
 
 	public static void main(String[] args) {
@@ -44,7 +33,8 @@ public class UiApplication {
 		protected void configure(HttpSecurity http) throws Exception {
 			// @formatter:off
 			http
-				.httpBasic().and()
+				.formLogin().and()
+				.logout().and()
 				.authorizeRequests()
 					.antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
 					.anyRequest().authenticated()
